bee7c94fac0c25b51e393a0abb3dce802f3a8f17
VanBac65 <vanbac6500@gmail.com>
2022-06-13 12:55:41 +0700

    up

---
+++ style.js
@@ -0,0 +1,25 @@
+
+
+// // function sdt() {
+// // 	document.getElementById('tooltip1').style.display="none";
+// // }
+
+// //  function hoten() {
+// // 	document.getElementById('tooltip').style.display="none";
+// //  }
+
+// var a = document.getElementById("name").value;
+// var phone = document.getElementById('sdt').value;
+// function ho_ten(){
+// 	if(document.getElementById("name").value!=null){
+// 		document.getElementById('tooltip').style.display="none"
+// 	}
+// 	else {
+// 		document.getElementById('tooltip').style.display="inherit"
+// 	}
+// }
+
+function button(){
+	if(document.getElementById('name').value!=null&&document.getElementById('phone').value)
+	alert('Gửi thông tin thành công');
+}
